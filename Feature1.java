e97e2f6b438d0ee8d764d480d0cb80bef07af1b0
IRIDIXVdt <larry_gu@126.com>
2021-09-17 18:30:22 +0800

    minor bug fix
    in Feature1

--- Feature1.java
+++ Feature1.java
@@ -2,7 +2,7 @@ public class Feature1{
     public static int lengthOfLongestSubstring(String s) {
         //this returns the Longest Substring Without Repeating Characters
         //this is my solution from a question in leetcode.com
-        return s;
+        return 0;
         
         
     }
