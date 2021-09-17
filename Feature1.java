d37617c07b078eb215d9b29005181c5f93cf1cc2
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-09-17 18:25:21 +0800

    Create Feature1.java

---
+++ Feature1.java
@@ -0,0 +1,10 @@
+public class Feature1{
+    public static int lengthOfLongestSubstring(String s) {
+        //this returns the Longest Substring Without Repeating Characters
+        //this is my solution from a question in leetcode.com
+        return s;
+        
+        
+    }
+
+}
