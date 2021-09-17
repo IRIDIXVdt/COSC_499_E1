a6db98076768b3fcf073d296e20916c7fd0d8565
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-09-17 18:26:03 +0800

    Create Feature2.java

---
+++ Feature2.java
@@ -0,0 +1,8 @@
+public class Feature2{
+    //this returns the longest palindromic substring in a given string s,
+    //this is my solution from a question in leetcode.com
+    public static String longestPalindrome(String s) {
+        return s;
+    }
+
+}
